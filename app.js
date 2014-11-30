3df455910406128151ef5f0eacb35095547d9e51
fuqcool <fuqcool@gmail.com>
2014-11-29 23:41:28 -0500

    connection string

--- app.js
+++ app.js
@@ -15,23 +15,13 @@ var partials = require('express-partials');
 var flash = require('connect-flash');
 
 var sessionStore = new MongoStore({
-						db : settings.db
-					}, function() {
-							console.log('connect mongodb success...');
-					});
+  url: settings.connectionString
+}, function() {
+  console.log('connect mongodb success...');
+});
 
 
 var app = express();
-// default to a 'localhost' configuration:
-var connection_string = '127.0.0.1:27017/sabor';
-// if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
-}
 
 app.configure(function(){
 	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
@@ -48,7 +38,7 @@ app.configure(function(){
 	app.use(express.methodOverride());
 
 	app.use(express.cookieParser());
-	
+
 	app.use(express.session({
 		secret : settings.cookie_secret,
 		cookie : {
@@ -76,24 +66,5 @@ app.get('/logout', routes.logout);
 
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-	console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port'));
 });
-
-
-/**
-* node-mongodb-native   https://github.com/christkv/node-mongodb-native
-* 
-* ejs	https://github.com/visionmedia/ejs
-* 
-* express-Migrating from 2.x to 3.x		https://github.com/visionmedia/express/wiki/Migrating-from-2.x-to-3.x
-* 
-* Express 3.x + Socket.IO		http://blog.lyhdev.com/2012/07/nodejs-express-3x-socketio.html
-* 
-* express-partials		https://github.com/publicclass/express-partials
-* 
-* connect-flash		https://github.com/jaredhanson/connect-flash
-*
-* connect-mongodb		https://github.com/masylum/connect-mongodb
-*
-* 
-*/
--- settings.js
+++ settings.js
@@ -1,6 +1,13 @@
+var env = process.env
+var connectionString
+
+if (env.OPENSHIFT_APP_NAME) {
+  connectionString = env.OPENSHIFT_MONGODB_DB_URL + env.OPENSHIFT_APP_NAME
+} else {
+  connectionString = '127.0.0.1:27017/sabor'
+}
+
 module.exports = {
-	cookie_secret : 'secret_meteoric',
-	db : 'sabor',
-	host : process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost',
-	port : process.env.OPENSHIFT_NODEJS_PORT || 27017
+  cookie_secret : 'secret_meteoric',
+  connectionString: connectionString
 }
