21d91af1c34286b5e1a0c753f6f51e41b6565617
fuqcool <fuqcool@gmail.com>
2014-11-30 00:58:02 -0500

    dbdbdb

--- models/post.js
+++ models/post.js
@@ -1,79 +1,59 @@
 var mongodb = require('./db');
 
 function Post(username, post, time) {
-	this.user = username;
-	this.post = post;
-	if (time) {
-		this.time = time;
-	} else {
-		this.time = new Date();
-	}
+  this.user = username;
+  this.post = post;
+  if (time) {
+    this.time = time;
+  } else {
+    this.time = new Date();
+  }
 };
 module.exports = Post;
 
 Post.prototype.save = function save(callback) {
-	// save into mongodb document
-	var post = {
-		user: this.user,
-		post: this.post,
-		time: this.time
-	};
+  // save into mongodb document
+  var post = {
+    user: this.user,
+    post: this.post,
+    time: this.time
+  };
 
-	mongodb.open(function(err, db) {
-		if (err) {
-		  return callback(err);
-		}
-		
-		db.collection('posts', function(err, collection) {
-			if (err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.ensureIndex('user');
-			
-			collection.insert(post, {safe: true}, function(err, post) {
-				mongodb.close();
-				callback(err, post);
-			});
-		});
-	});
+  mongodb(function(db) {
+    db.collection('posts', function(err, collection) {
+      collection.ensureIndex('user');
+
+      collection.insert(post, {safe: true}, function(err, post) {
+	callback(err, post);
+      });
+    });
+  });
 };
 
 Post.get = function get(username, callback) {
-	mongodb.open(function(err, db) {
-		if (err) {
-			return callback(err);
-		}
-	
-		db.collection('posts', function(err, collection) {
-			if (err) {
-				mongodb.close();
-				return callback(err);
-			}
+  mongodb(function (db) {
+    db.collection('posts', function(err, collection) {
+      //find documents whose user attribute is 'username', if null then match all
+      //查找user属性为username的文档，如果username为null则匹配全部
+      var query = {};
+      if (username) {
+	query.user = username;
+      }
 
-			//find documents whose user attribute is 'username', if null then match all
-			//查找user属性为username的文档，如果username为null则匹配全部
-			var query = {};
-			if (username) {
-				query.user = username;
-			}
+      collection.find(query, {limit:9}).sort({time: -1}).toArray(function(err, docs) {
+	if (err) {
+	  callback(err, null);
+	}
 
-			collection.find(query, {limit:9}).sort({time: -1}).toArray(function(err, docs) {
-				mongodb.close();
+	var posts = [];
 
-				if (err) {
-					callback(err, null);
-				}
-
-				var posts = [];
-				
-				docs.forEach(function(doc, index) {
-					var post = new Post(doc.user, doc.post, doc.time);
-					posts.push(post);
-				});
-
-				callback(null, posts);
-			});
-		});
+	docs.forEach(function(doc, index) {
+	  var post = new Post(doc.user, doc.post, doc.time);
+	  posts.push(post);
 	});
+
+	callback(null, posts);
+      });
+    });
+  });
 };
